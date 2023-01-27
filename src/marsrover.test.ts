ce1c3d8ee6856a1ea626453249f3689b8b25bab9
Sugandha <raosugandha12@gmail.com>
2023-01-27 07:51:46 +0000

    corrected input file name

--- src/marsrover.test.ts
+++ src/marsrover.test.ts
@@ -1,21 +1,21 @@
-import MarsRover from './marsRover';
+import MarsRover from './marsrover';
 
 describe('Mars Rover', () => {
-    it.each`instructions | expectedOrientation 
+    it.each `instruction | expectedOrientation
             ${'L'}       | ${'0:0:W'}
             ${'LL'}      | ${'0:0:S'}
             ${'LLL'}     | ${'0:0:E'}
-    `('should be in position $expectedOrientation when given $instructions',
-        ({instructions, expectedOrientation}) => {
+    `('should be in position expectedOrientation when given instruction',
+        ({instruction, expectedOrientation}) => {
             const marsRover = new MarsRover();
 
-            expect(marsRover.executeInstructions(instructions)).toBe(expectedOrientation);
+            expect(marsRover.executeInstructions(instruction)).toBe(expectedOrientation);
         });
 
     it('should be in position 0:1:N when given instruction M', () => {
         const marsRover = new MarsRover();
 
-        expect(marsRover.executeInstructions('M')).toBe('0:1:N');
+        expect(marsRover.executeInstructions('M')).toBe('0:1:N'); 
     })
 
     it('should be in position 1:0:N when given instruction RM', () => {
@@ -41,10 +41,6 @@ describe('Mars Rover', () => {
 
         expect(marsRover.executeInstructions('RMMMMMMMMMM')).toBe('0:0:E');
     })
-     it('should be in position 1:2:N when given instruction LMLMLMLMM', () => {
-        const marsRover = new MarsRover();
-
-        expect(marsRover.executeInstructions('LMLMLMLMM')).toBe('1:2:N');
-    })
+    
    
 })  
